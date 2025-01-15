f9a3513ddfbeab25ce6a516fd101201cdccb8579
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-01-15 19:48:45 +0300

    Revert "fix(fragments): add key"
    This reverts commit ae15b22202320dd65b3d7b852f9a410d1c9f1845.

--- fragments/footer-fragment/src/footer-component.tsx
+++ fragments/footer-fragment/src/footer-component.tsx
@@ -82,15 +82,12 @@ export const Footer: FC<FooterProps> = memo(({
               <SocialLinks linkTelegram={linkTelegram} linkVk={linkVk} />
             </Row>
             <Column width='100%' paddingY='$g24' gap='$g20' display={['flex', 'flex', 'none']}>
-              {navigationItems.map((
-                {
-                  contentAddons: { title, content },
-                }: {
-                  contentAddons: { title: string; content: string }
-                },
-                index: number
-              ) => (
-                <Box key={`${index}`}>
+              {navigationItems.map(({
+                contentAddons: { title, content },
+              }: {
+                contentAddons: { title: string; content: string }
+              }) => (
+                <Box>
                   <NextNavLink key={title} path={content}>
                     <Text fontWeight='$medium'>{title}</Text>
                   </NextNavLink>
