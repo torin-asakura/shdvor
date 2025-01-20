b7f64b7d350419f9435c69b00ff034a83868aaea
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-01-20 13:15:07 +0300

    feat(globals): helper interfaces

--- globals/data/src/interfaces/helpers.interface.ts
+++ globals/data/src/interfaces/helpers.interface.ts
@@ -1,2 +1,6 @@
 export type ArrayElement<A> = A extends ReadonlyArray<infer T> ? T : never
 export type NonNullable<T> = Exclude<T, null | undefined>
+
+export type NonNullableObject<T> = NonNullable<{ [P in keyof T]-?: NonNullableObject<T[P]> }>
+
+export type SetState<T> = React.Dispatch<React.SetStateAction<T>>
