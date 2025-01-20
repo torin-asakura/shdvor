adfeeb24fca03c009eabc09524b63c09dd657e84
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-01-20 21:05:09 +0300

    feat(fragments): not-found-fragment

---
+++ fragments/not-found-fragment/src/not-found-fragment.component.tsx
@@ -0,0 +1,15 @@
+import type { FC }          from 'react'
+
+import React                from 'react'
+import { FormattedMessage } from 'react-intl'
+
+import { Box }              from '@ui/layout'
+import { Text }             from '@ui/text'
+
+export const NotFoundFragment: FC = () => (
+  <Box width='$fill' maxWidth='$g1440' marginTop={[112, 112, 152]} paddingX={[20, 20, 83]}>
+    <Text fontWeight='$medium' fontSize='$giant' lineHeight='$grown' color='$black'>
+      <FormattedMessage id='not-found' />
+    </Text>
+  </Box>
+)
