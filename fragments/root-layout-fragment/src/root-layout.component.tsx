817b12f5cc43a058eb8048fdb25faaffc131a9a3
Nikita Elfimov <nelfimov@mail.ru>
2025-04-08 09:21:45 +0300

    feat(site): redirect to new url (#196)
    feat(site): redirect to new url
    
    feat(site): redirect to new url

--- fragments/root-layout-fragment/src/root-layout.component.tsx
+++ fragments/root-layout-fragment/src/root-layout.component.tsx
@@ -26,9 +26,16 @@ const ibmPlexSans = IBM_Plex_Sans({
 
 export const RootLayout: FC<PropsWithChildren<RootLayoutProps>> = memo(({ children, messages }) => {
   const gaTrackingId = process.env.GA_TRACKING_ID || 'GTM-T267QVHF'
+  // TODO: remove once moved to new site
+  const redirectUrl = process.env.SITE_ORIGIN
 
   return (
     <html className={`${ibmPlexSans.className} ${ibmPlexSans.variable}`}>
+      {redirectUrl && 
+        <header>
+          <meta http-equiv="refresh" content="0;url=https://shdvor.pro" />
+        </header>
+      }
       <body id={GLOBAL_THEME_ID}>
         <ApolloWrapper>
           <RootLayoutProviders messages={messages}>
